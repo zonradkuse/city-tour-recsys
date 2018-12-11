4ac2ac91ea642af64a5d650309b380a376d5a104
Jasmina Bajramovic <jbajramovi1@etf.unsa.ba>
2018-12-11 22:39:52 +0100

    add strategy for using cold-start or user recom

--- recommender_core/recommendation.py
+++ recommender_core/recommendation.py
@@ -1,3 +1,4 @@
+import connection_provider
 import recommender_core.coldstart as coldstart
 import recommender_core.userrecommender as userrecommender
 from recommender_core.Recommender import initialize_recommender
@@ -5,6 +6,24 @@ from recommender_core.Recommender import initialize_recommender
 def recommend(user, city):
     # TODO decide which strategy to choose, for now just use the coldstart
 
-    coldstart_recommender = initialize_recommender(coldstart.ColdStartRecommender)
-    user_recommender = initialize_recommender(userrecommender.UserRecommender)
-    return user_recommender.recommend(city, user)
+    if (choose_user_recommendation(user)==True):
+        user_recommender = initialize_recommender(userrecommender.UserRecommender)
+        return user_recommender.recommend(city, user)
+    else:
+        coldstart_recommender = initialize_recommender(coldstart.ColdStartRecommender)
+        return coldstart_recommender.recommend(city,user)
+
+
+def choose_user_recommendation(user):
+    conn = connection_provider.get_fresh()
+    cursor = conn.cursor()
+    print(user)
+    cursor.execute("select REVIEW_ID FROM REVIEWS WHERE USER='{}'".format(user))
+    result = cursor.fetchone()
+    print(result)
+    conn.close()
+
+    if result == None:
+        return False
+    else:
+        return True
