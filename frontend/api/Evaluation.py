33a790937095abea3ba3556ac776a004f1df80c5
Jasmina Bajramovic <jbajramovi1@etf.unsa.ba>
2018-12-11 16:56:47 +0100

    user review

--- frontend/api/Evaluation.py
+++ frontend/api/Evaluation.py
@@ -2,14 +2,20 @@ from frontend.api.App import app
 
 from flask import request
 
+import connection_provider
+
 import json
 
-@app.route('/api/v1/evaluate/<nodeid>', methods = ["PUT"])
+@app.route('/api/v1/review/<nodeid>', methods = ["PUT"])
 def evaluate(nodeid):
     # return an object or list with different recommendations
-    value = request.json['evaluation']
+    value = request.json['review']
     user = request.json['username']
 
-    # TODO write this to database
+    conn = connection_provider.get_fresh()
+    cursor = conn.cursor()
+    cursor.execute("insert into REVIEWS(NODE_ID,USER,REVIEW_VALUE) VALUES (?,?,?)",(nodeid,user,value));
+    conn.commit()
+    conn.close()
 
-    return json.dumps(value)
+    return 'OK'
